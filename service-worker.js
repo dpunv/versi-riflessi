a3dce87ba6f834fd68b3f65d0799c10750178e76
Davide Pirrò <39238291+dpunv@users.noreply.github.com>
2025-10-01 17:00:16 +0200

    add images, service worker e manifest for PWA and add 3 poesie

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = "versi-riflessi-v1";
+const OFFLINE_URLS = [
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(OFFLINE_URLS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached =>
+      cached ||
+      fetch(event.request).catch(() => caches.match("./index.html"))
+    )
+  );
+});
